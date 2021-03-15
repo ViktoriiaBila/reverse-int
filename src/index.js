e8ef6d210f199d72bc7746387cc02c1bbb58322a
Vika <vika.bila97@gmail.com>
2021-03-15 13:02:22 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	/*
+		n = (n < 0) ? n * (-1) : n;
+		let arr = (String(n).split('')).reverse();
+		let result = '';
+		for (let i = 0; i < arr.length; i++) result += arr[i];
+		return Number(result);
+	*/
+	return Number(((String((n < 0) ? n * (-1) : n).split('')).reverse()).reduce((a, c) => a += c));
 }
+
